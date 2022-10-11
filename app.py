4afbc4e8104e96b2ea3d99533facd3e471f0df99
Ming-Jer Lee <mingjerli@gmail.com>
2022-10-10 21:37:28 -0400

    origins

--- app.py
+++ app.py
@@ -4,8 +4,23 @@ from fastapi import FastAPI
 from price_change_all_regions_module import get_all_regions
 from price_change_all_regions_module import get_price_change
 from price_change_all_regions_module import get_df_long_for_artifact_all_regions_and_downstream
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {"message": "Welcome to the API!"}
 
 @app.get("/regions/")
 def all_regions():
